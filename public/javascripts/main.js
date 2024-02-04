a852e0cd566caa60d29a3c850513d92e72cb8a72
im2013 <im2013@hw.ac.uk>
2024-02-04 18:23:21 +0000

    new brainbox

---
+++ public/javascripts/main.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css';
+$(function () {
+    const socket = io();
+    $('form').submit(function(e) {
+      e.preventDefault(); // prevents page reloading
+      socket.emit('chat message', $('#m').val());
+      $('#m').val('');
+      return false;
+    });
+    socket.on('chat message', function(msg){
+      $('#messages').append($('<li>').text(msg));
+    });
+  });
---
+++ public/vue/textbox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <label for="niceTextBox">Nicer Text Box:</label>
+    <input
+      type="text"
+      class="form-control"
+      id="niceTextBox"
+      v-model="textBoxValue"
+      placeholder="Enter text..."
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      textBoxValue: '',
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* Add any custom styles if needed */
+</style>
